bc0a96f676c7f546dce85c93f3d78007d96d9804
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-05 06:51:27 -0400

    rename pc* 0d*

--- Makefile
+++ Makefile
@@ -13,11 +13,11 @@ npmstuff:
 	npm install js-beautify
 
 js:
-	./fab/fab - PseudoCode pc.ohm jspc.fmt --support='./jssupport.js' <container.0d | sed -e '/^$$/d' >container.js
+	./fab/fab - PseudoCode 0d.ohm js0d.fmt --support='./jssupport.js' <container.0d | sed -e '/^$$/d' >container.js
 	~/node_modules/js-beautify/js/bin/js-beautify.js container.js
 
 dev:
-	./fab/fab - PseudoCode pc.ohm jspc.fmt --support='./jssupport.js' <test0.0d | sed -e '/^$$/d' >test0.js
+	./fab/fab - PseudoCode 0d.ohm js0d.fmt --support='./jssupport.js' <test0.0d | sed -e '/^$$/d' >test0.js
 	~/node_modules/js-beautify/js/bin/js-beautify.js test0.js
 
 
--- container.js
+++ container.js
@@ -105,28 +105,26 @@ child.step (sendProcedure);
 });
 }
 function route (sendProcedure) {
-this.routingMap.forAll (connection => {
-synonym;
-deferred__message__stack;
-[];
 {
+var deferred__message__stack = [];
+this.routingMap.forAll (connection => {
 ;
 connection.direction;
 {
 across;
 up;
 {
-synonym;
-output__message;
-connection.sender.outputs;
 {
-deferred__message__stack.push (???ListOfObjects{targetmessage}[{connection.receivermessage}]);}}
+var output__message = connection.sender.outputs;
+deferred__message__stack.push (???ListOfObjects{targetmessage}[{connection.receivermessage}]);
+}}
 down;
 passThrough;
 {
-}}}
+}}
 });
 deferred__message__stack.forAll (pair => {
 pair.target.handle (pair.messagesendProcedure);
 });
+}
 }
